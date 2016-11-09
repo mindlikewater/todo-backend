9bbaab696f82bc1d138e87a0ca6774cea9f14522
Tai Chen <mindlikewater@gmail.com>
2016-11-09 15:25:38 -0500

    Updated user controller with ways to add users and get users

---
+++ app/Http/Controllers/UserController.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const User = use("App/Model/User")
+
+class UserController {
+
+  * create (request, response) {
+    let data = request.only('email', 'name')
+    let user = yield User.create(data)
+
+    response.json(user)
+  }
+
+  * show (request, response) {
+    let userId = request.param('id')
+    let user = yield User.find(userId)
+
+    if (user) {
+      response.json(user)
+    } else {
+      response.status(404)
+      response.json({ error: "No such user" })
+    }
+  }
+
+}
+
+module.exports = UserController
