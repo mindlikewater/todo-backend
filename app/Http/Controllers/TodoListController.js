94bcc57ae86cd567a05e94bff0dc2466566defe9
Tai Chen <mindlikewater@gmail.com>
2016-11-09 15:08:17 -0500

    Added methods for creating users and getting users

--- app/Http/Controllers/TodoListController.js
+++ app/Http/Controllers/TodoListController.js
@@ -6,6 +6,21 @@ class TodoListController {
     response.json({ message: "hello world" })
   }
 
+  * create (request, response) {
+    let userId = request.param('id')
+    let data = request.only('name')
+    data.user_id = userId
+
+    let todoList = yield TodoList.create(data)
+    response.status(201).json(todoList)
+  }
+
+  * index (request, response) {
+    let userId = request.param('id')
+    let lists = yield TodoList.query().where('user_id', userId)
+
+    response.json(lists)
+  }
 }
 
 module.exports = TodoListController
