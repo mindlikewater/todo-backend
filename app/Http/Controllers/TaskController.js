2faf1e89cfcb698e4bc63c8f3e5e1ae1df117743
Tai Chen <mindlikewater@gmail.com>
2016-11-10 09:01:17 -0500

    Imported task model to task controller

--- app/Http/Controllers/TaskController.js
+++ app/Http/Controllers/TaskController.js
@@ -1,9 +1,12 @@
 'use strict'
 
+const TodoList = use("App/Model/Task")
+
+
 class TaskController {
 
   * create (request, response) {
-    let data = request.only('title', 'due_date', 'done', 'todo_list_id')
+    let data = request.only('title', 'todo_list_id', 'due_date', 'done')
     let task = yield Task.create(data)
 
     response.status(201).json(task)
@@ -11,7 +14,7 @@ class TaskController {
 
   * index (request, response) {
     let listId = request.param('list_id')
-    let tasks = yield.Task.query().where('todo_list_id', listId)
+    let tasks = yield Task.query().where('todo_list_id', listId)
 
     response.json(tasks)
   }
@@ -19,18 +22,19 @@ class TaskController {
   * update (request, response) {
     let taskId = request.param('task_id')
     let task = yield Task.findBy('id', taskId)
-    let data = request.only('title', 'list_id', 'due_date', 'done')
+    let data = request.only('title', 'todo_list_id', 'due_date', 'done')
 
     task.fill(data)
 
     yield task.save()
-    response.json(task)
+    response.status(201).json(task)
   }
 
   * delete (request, response) {
     let taskId = request.param('task_id')
-    let deleteTask = yield Task.findBy('id', taskId)
-    yield deleteTask.del()
+    let deleteTask = yield Task.findBy('id', taskId).del()
+
+    response.status(201).json({ success: "Task successfully deleted" })
   }
 
 }
